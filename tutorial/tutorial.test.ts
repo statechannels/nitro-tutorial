4b19a921911d910f54f09263a43ceccb3e94f3b1
George Knee <georgeknee@googlemail.com>
2020-05-27 16:28:51 +0100

    Add Lesson 6
    Conclude a channel (happy)

--- tutorial/tutorial.test.ts
+++ tutorial/tutorial.test.ts
@@ -11,6 +11,7 @@ import {
   getFixedPart,
   validTransition,
   signState,
+  hashOutcome,
 } from "@statechannels/nitro-protocol";
 const getDepositedEvent = (events) =>
   events.find(({ event }) => event === "Deposited").args;
@@ -31,6 +32,9 @@ import {
   signStates,
 } from "@statechannels/nitro-protocol/lib/test/test-helpers";
 import { HashZero, AddressZero } from "ethers/constants";
+
+// TODO hoist these up to the module export in nitro-protocol
+import { hashAppPart } from "@statechannels/nitro-protocol/lib/src/contract/state";
 
 // Set up an interface to the deployed Asset Holder Contract
 beforeAll(async () => {
@@ -232,6 +236,51 @@ describe("Tutorial", () => {
       isFinalCount,
       sigs,
       whoSignedWhat
+    );
+    await (await tx).wait();
+  });
+
+  it("Lesson 6: Conclude a channel (happy)", async () => {
+    const whoSignedWhat = [0, 0, 0];
+    const largestTurnNum = 4;
+    const participants = [];
+    const wallets: Wallet[] = [];
+    for (let i = 0; i < 3; i++) {
+      wallets[i] = Wallet.createRandom();
+      participants[i] = wallets[i].address;
+    }
+    const chainId = "0x1234";
+    const channelNonce = bigNumberify(0).toHexString();
+    const channel: Channel = { chainId, channelNonce, participants };
+
+    const state: State = {
+      isFinal: false, // FIXME
+      channel,
+      outcome: [],
+      appDefinition: AddressZero,
+      appData: HashZero,
+      challengeDuration: 1,
+      turnNum: largestTurnNum,
+    };
+
+    // Sign the states
+    const sigs = await signStates([state], wallets, whoSignedWhat);
+
+    /*
+     * Conclude
+     */
+    const numStates = 1;
+    const fixedPart = getFixedPart(state);
+    const appPartHash = hashAppPart(state);
+    const outcomeHash = hashOutcome(state.outcome);
+    const tx = NitroAdjudicator.conclude(
+      largestTurnNum,
+      fixedPart,
+      appPartHash,
+      outcomeHash,
+      numStates,
+      whoSignedWhat,
+      sigs
     );
     await (await tx).wait();
   });
