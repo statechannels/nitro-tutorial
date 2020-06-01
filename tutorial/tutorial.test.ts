b7a80b77d7146731a5b50a80ab4680d91941b0bd
George Knee <georgeknee@googlemail.com>
2020-06-01 11:12:04 +0100

    Add Lesson 14
    Call transferAll

--- tutorial/tutorial.test.ts
+++ tutorial/tutorial.test.ts
@@ -53,6 +53,7 @@ import {
   AssetOutcome,
   AllocationAssetOutcome,
   GuaranteeAssetOutcome,
+  encodeAllocation,
 } from "@statechannels/nitro-protocol/lib/src/contract/outcome";
 
 let provider: ethers.providers.JsonRpcProvider;
@@ -672,7 +673,7 @@ describe("Tutorial", () => {
     expect(decodeOutcome(encodeOutcome(outcome))).toEqual(outcome);
   });
 
-  it.only("Lesson 13: Call pushOutcome", async () => {
+  it("Lesson 13: Call pushOutcome", async () => {
     // BEGIN LESSON SETUP
     const whoSignedWhat = [0, 0, 0];
     const largestTurnNum = 4;
@@ -739,5 +740,117 @@ describe("Tutorial", () => {
     );
 
     await (await tx1).wait();
+  });
+
+  it.only("Lesson 14: Call transferAll", async () => {
+    // BEGIN LESSON SETUP
+    const amount = "0x03";
+
+    const EOA = ethers.Wallet.createRandom().address;
+    const destination = hexZeroPad(EOA, 32);
+
+    const assetOutcome: AllocationAssetOutcome = {
+      assetHolderAddress: process.env.ETH_ASSET_HOLDER_ADDRESS,
+      allocationItems: [{ destination, amount }],
+    };
+
+    const whoSignedWhat = [0, 0, 0];
+    const largestTurnNum = 4;
+    const participants = [];
+    const wallets: Wallet[] = [];
+    for (let i = 0; i < 3; i++) {
+      wallets[i] = Wallet.createRandom();
+      participants[i] = wallets[i].address;
+    }
+    const chainId = "0x1234";
+    const channelNonce = bigNumberify(0).toHexString();
+    const channel: Channel = { chainId, channelNonce, participants };
+    const channelId = getChannelId(channel);
+
+    /*
+      Attempt to deposit 1 wei against the channel id we created.
+      Inspect the error message in the console for a hint about the bug on the next line 
+    */
+    const tx0 = ETHAssetHolder.deposit(channelId, 0, amount, {
+      value: amount,
+    });
+
+    await (await tx0).wait();
+
+    const state: State = {
+      isFinal: true,
+      channel,
+      outcome: [assetOutcome],
+      appDefinition: AddressZero,
+      appData: HashZero,
+      challengeDuration: 1,
+      turnNum: largestTurnNum,
+    };
+
+    // Sign the states
+    const sigs = await signStates([state], wallets, whoSignedWhat);
+
+    /*
+     * Conclude
+     */
+    const numStates = 1;
+    const fixedPart = getFixedPart(state);
+    const appPartHash = hashAppPart(state);
+    const outcomeHash = hashOutcome(state.outcome);
+
+    const tx1 = NitroAdjudicator.conclude(
+      largestTurnNum,
+      fixedPart,
+      appPartHash,
+      outcomeHash,
+      numStates,
+      whoSignedWhat,
+      sigs
+    );
+    const receipt = await (await tx1).wait();
+
+    const turnNumRecord = 0; // Always 0 for a happy conclude
+    const finalizesAt = (await provider.getBlock(receipt.blockNumber))
+      .timestamp;
+    const stateHash = HashZero; // Reset in a happy conclude
+    const challengerAddress = AddressZero; // Reset in a happy conclude
+    const outcomeBytes = encodeOutcome(state.outcome);
+
+    const tx2 = NitroAdjudicator.pushOutcome(
+      channelId,
+      turnNumRecord,
+      finalizesAt,
+      stateHash,
+      challengerAddress,
+      outcomeBytes
+    );
+
+    await (await tx2).wait();
+
+    // END LESSON SETUP
+
+    // BEGIN LESSON 14 proper
+
+    const tx3 = ETHAssetHolder.transferAll(
+      channelId,
+      encodeAllocation(assetOutcome.allocationItems)
+    );
+
+    const { events } = await (await tx3).wait();
+
+    expect(events).toMatchObject([
+      {
+        event: "AssetTransferred",
+        args: {
+          channelId,
+          destination: destination.toLowerCase(),
+          amount: { _hex: amount },
+        },
+      },
+    ]);
+
+    expect(
+      bigNumberify(await provider.getBalance(EOA)).eq(bigNumberify(amount))
+    );
   });
 });
