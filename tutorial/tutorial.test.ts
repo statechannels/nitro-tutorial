6340de6363dbdc07d3e1c86617153fa4eee5f7f8
George Knee <georgeknee@googlemail.com>
2020-06-01 17:53:06 +0100

    Add Lessson 15
    call claimAll
    
    and break it

--- tutorial/tutorial.test.ts
+++ tutorial/tutorial.test.ts
@@ -54,6 +54,7 @@ import {
   AllocationAssetOutcome,
   GuaranteeAssetOutcome,
   encodeAllocation,
+  encodeGuarantee,
 } from "@statechannels/nitro-protocol/lib/src/contract/outcome";
 
 let provider: ethers.providers.JsonRpcProvider;
@@ -742,7 +743,7 @@ describe("Tutorial", () => {
     await (await tx1).wait();
   });
 
-  it.only("Lesson 14: Call transferAll", async () => {
+  it("Lesson 14: Call transferAll", async () => {
     // BEGIN LESSON SETUP
     const amount = "0x03";
 
@@ -852,6 +853,177 @@ describe("Tutorial", () => {
 
     expect(
       bigNumberify(await provider.getBalance(EOA)).eq(bigNumberify(amount))
+    );
+  });
+
+  it("Lesson 15: Call claimAll", async () => {
+    // BEGIN LESSON SETUP
+    const amount = "0x03";
+    const EOA1 = ethers.Wallet.createRandom().address;
+    const EOA2 = ethers.Wallet.createRandom().address;
+    const destination1 = hexZeroPad(EOA1, 32);
+    const destination2 = hexZeroPad(EOA2, 32);
+
+    const assetOutcomeForTheTargetChannel: AllocationAssetOutcome = {
+      assetHolderAddress: process.env.ETH_ASSET_HOLDER_ADDRESS,
+      allocationItems: [
+        { destination: destination1, amount },
+        { destination: destination2, amount },
+      ],
+    };
+
+    const whoSignedWhat = [0, 0, 0];
+    const largestTurnNum = 4;
+    const participants = [];
+    const wallets: Wallet[] = [];
+    for (let i = 0; i < 3; i++) {
+      wallets[i] = Wallet.createRandom();
+      participants[i] = wallets[i].address;
+    }
+    const chainId = "0x1234";
+    const channelNonce = bigNumberify(0).toHexString();
+    const targetChannel: Channel = { chainId, channelNonce, participants };
+    const targetChannelId = getChannelId(targetChannel);
+
+    // Finalise and pushOutcome for target channel
+    const state: State = {
+      isFinal: true,
+      channel: targetChannel,
+      outcome: [assetOutcomeForTheTargetChannel],
+      appDefinition: AddressZero,
+      appData: HashZero,
+      challengeDuration: 1,
+      turnNum: largestTurnNum,
+    };
+    let sigs = await signStates([state], wallets, whoSignedWhat);
+    let numStates = 1;
+    let fixedPart = getFixedPart(state);
+    let appPartHash = hashAppPart(state);
+    let outcomeHash = hashOutcome(state.outcome);
+
+    const tx0 = NitroAdjudicator.conclude(
+      largestTurnNum,
+      fixedPart,
+      appPartHash,
+      outcomeHash,
+      numStates,
+      whoSignedWhat,
+      sigs
+    );
+    const receipt = await (await tx0).wait();
+    let turnNumRecord = 0; // Always 0 for a happy conclude
+    let finalizesAt = (await provider.getBlock(receipt.blockNumber)).timestamp;
+    let stateHash = HashZero; // Reset in a happy conclude
+    let challengerAddress = AddressZero; // Reset in a happy conclude
+    let outcomeBytes = encodeOutcome(state.outcome);
+    const tx1 = NitroAdjudicator.pushOutcome(
+      targetChannelId,
+      turnNumRecord,
+      finalizesAt,
+      stateHash,
+      challengerAddress,
+      outcomeBytes
+    );
+    await (await tx1).wait();
+
+    // Finalise and pushOutcome for the guarantor channel
+
+    const assetOutcomeForTheGuarantorChannel: GuaranteeAssetOutcome = {
+      assetHolderAddress: process.env.ETH_ASSET_HOLDER_ADDRESS,
+      guarantee: {
+        targetChannelId: targetChannelId,
+        destinations: [
+          destination2,
+          destination1, // Note reversed order
+        ],
+      },
+    };
+
+    const guarantorChannel: Channel = {
+      chainId,
+      channelNonce: channelNonce + 1,
+      participants,
+    };
+    const guarantorChannelId = getChannelId(guarantorChannel);
+    const stateForGuarantorChannel: State = {
+      isFinal: true,
+      channel: guarantorChannel,
+      outcome: [assetOutcomeForTheGuarantorChannel],
+      appDefinition: AddressZero,
+      appData: HashZero,
+      challengeDuration: 1,
+      turnNum: largestTurnNum,
+    };
+    sigs = await signStates([stateForGuarantorChannel], wallets, whoSignedWhat);
+    numStates = 1;
+    fixedPart = getFixedPart(stateForGuarantorChannel);
+    appPartHash = hashAppPart(stateForGuarantorChannel);
+    outcomeHash = hashOutcome(stateForGuarantorChannel.outcome);
+
+    const tx2 = NitroAdjudicator.conclude(
+      largestTurnNum,
+      fixedPart,
+      appPartHash,
+      outcomeHash,
+      numStates,
+      whoSignedWhat,
+      sigs
+    );
+    const receiptFortx2 = await (await tx2).wait();
+    turnNumRecord = 0; // Always 0 for a happy conclude
+    finalizesAt = (await provider.getBlock(receiptFortx2.blockNumber))
+      .timestamp;
+    stateHash = HashZero; // Reset in a happy conclude
+    challengerAddress = AddressZero; // Reset in a happy conclude
+    outcomeBytes = encodeOutcome(stateForGuarantorChannel.outcome);
+    const tx3 = NitroAdjudicator.pushOutcome(
+      guarantorChannelId,
+      turnNumRecord,
+      finalizesAt,
+      stateHash,
+      challengerAddress,
+      outcomeBytes
+    );
+    await (await tx3).wait();
+
+    const tx4 = ETHAssetHolder.deposit(guarantorChannelId, 0, amount, {
+      value: amount,
+    });
+
+    await (await tx4).wait();
+
+    // END LESSON SETUP
+
+    // BEGIN LESSON 15 proper
+
+    // FIXME
+    const tx5 = ETHAssetHolder.claimAll(
+      guarantorChannelId,
+      encodeAllocation(assetOutcomeForTheTargetChannel.allocationItems),
+      encodeGuarantee(assetOutcomeForTheGuarantorChannel.guarantee)
+    );
+
+    // const tx5 = ETHAssetHolder.claimAll(
+    //   guarantorChannelId,
+    //   encodeGuarantee(assetOutcomeForTheGuarantorChannel.guarantee),
+    //   encodeAllocation(assetOutcomeForTheTargetChannel.allocationItems)
+    // );
+
+    const { events } = await (await tx5).wait();
+
+    expect(events).toMatchObject([
+      {
+        event: "AssetTransferred",
+        args: {
+          channelId: guarantorChannelId,
+          destination: destination2.toLowerCase(),
+          amount: { _hex: amount },
+        },
+      },
+    ]);
+
+    expect(
+      bigNumberify(await provider.getBalance(EOA2)).eq(bigNumberify(amount))
     );
   });
 });
