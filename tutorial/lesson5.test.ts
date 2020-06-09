962f89311af5d45d4a1b7fc51b88bda4f70d4088
George Knee <georgeknee@googlemail.com>
2020-06-09 15:22:54 +0100

    Neaten up lesson 5

--- tutorial/lesson5.test.ts
+++ tutorial/lesson5.test.ts
@@ -5,13 +5,15 @@ import {
 } from "@statechannels/nitro-protocol";
 import { ethers } from "ethers";
 
-// Set up an ethereum provider connected to our local blockchain
+/* Set up an ethereum provider connected to our local blockchain */
 const provider = new ethers.providers.JsonRpcProvider(
   `http://localhost:${process.env.GANACHE_PORT}`
 );
 
-// The contract has already been compiled and will be automatically deployed to a local blockchain
-// Import the compilation artifact so we can use the ABI to 'talk' to the deployed contract
+/* 
+  The ETHAssetHolder contract has already been compiled and will be automatically deployed to a local blockchain.
+  Import the compilation artifact so we can use the ABI to 'talk' to the deployed contract
+*/
 const {
   EthAssetHolderArtifact,
 } = require("@statechannels/nitro-protocol").ContractArtifacts;
@@ -24,30 +26,33 @@ const ETHAssetHolder = new ethers.Contract(
 it("Lesson 5: depositing into the ETH asset holder", async () => {
   /*
       Get an appropriate representation of 1 wei, and
-      use one of our helpers to quickly create a random channel id
-    */
-  const held = parseUnits("1", "wei");
-  const channelId = randomChannelId();
+      use one of our helpers to quickly create a 
+      random channel id for our deposit destination.
+      WARNING: don't do this in the wild: you won't be able to recover these funds.
+  */
+  const amount = parseUnits("1", "wei");
+  const destination = randomChannelId();
 
   /*
       Attempt to deposit 1 wei against the channel id we created.
       Inspect the error message in the console for a hint about the bug on the next line 
-    */
-  const tx0 = ETHAssetHolder.deposit(channelId, 0, held, {
-    value: held,
-  }); // FIXME
+  */
+  const expectedHeld = 1; // FIXME
+  const tx0 = ETHAssetHolder.deposit(destination, expectedHeld, amount, {
+    value: amount,
+  });
 
   /* 
       Expectations around the event that should be emitted on a successfull deposit, and 
       the new value of the public 'holdings' storage on chain:
-    */
+  */
   const { events } = await (await tx0).wait();
   const depositedEvent = getDepositedEvent(events);
 
-  expect(await ETHAssetHolder.holdings(channelId)).toEqual(held);
+  expect(await ETHAssetHolder.holdings(destination)).toEqual(amount);
   expect(depositedEvent).toMatchObject({
-    channelId,
-    amountDeposited: held,
-    destinationHoldings: held,
+    destination,
+    amountDeposited: amount,
+    destinationHoldings: amount,
   });
 });
