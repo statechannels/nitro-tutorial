3062418358f6897c3a9b46d5d920d9f563e4a1d4
geoknee <georgeknee@googlemail.com>
2020-07-27 10:51:52 +0100

    Ensure contract addresses are always the same

--- deployment/deploy.js
+++ deployment/deploy.js
@@ -4,10 +4,23 @@ const {
   TrivialAppArtifact,
 } = require("@statechannels/nitro-protocol").ContractArtifacts;
 
-const { GanacheDeployer } = require("@statechannels/devtools");
+const {
+  GanacheDeployer,
+  ETHERLIME_ACCOUNTS,
+} = require("@statechannels/devtools");
 
 const deploy = async () => {
-  const deployer = new GanacheDeployer(Number(process.env.GANACHE_PORT));
+  const deployer = new GanacheDeployer(
+    Number(process.env.GANACHE_PORT),
+    ETHERLIME_ACCOUNTS[0].privateKey
+  );
+
+  const txCount = await deployer.etherlimeDeployer.signer.getTransactionCount();
+  if (txCount > 0) {
+    throw new Error(
+      `The tutorial requires a fresh blockchain in order for test assertions to be accurate. Try killing anything running on port ${process.env.GANACHE_PORT} and trying again`
+    );
+  }
 
   const NITRO_ADJUDICATOR_ADDRESS = await deployer.deploy(
     NitroAdjudicatorArtifact
